a3425c4d25c816ae6b78eac21a2ad113ffef38b3
mediaselect <media.select.fr@gmail.com>
2025-02-07 09:09:32 +0100

    Initial commit

---
+++ freeboxos.py
@@ -0,0 +1,461 @@
+import json
+import keyring
+import logging
+import os
+import shutil
+import sentry_sdk
+import sys
+
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+from time import sleep
+from logging.handlers import RotatingFileHandler
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    WebDriverException,
+    ElementNotInteractableException,
+    ElementClickInterceptedException
+)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+from channels_free import CHANNELS_FREE
+from module_freeboxos import is_snap_installed, is_firefox_snap, is_firefox_esr_installed
+
+user = os.getenv("USER")
+
+sys.path.append("/home/" + user + "/.config/select_freeboxos")
+
+from config import (
+    ADMIN_PASSWORD,
+    FREEBOX_SERVER_IP,
+    MEDIA_SELECT_TITLES,
+    MAX_SIM_RECORDINGS,
+    SENTRY_MONITORING_SDK,
+    CRYPTED_CREDENTIALS,
+)
+
+month_names_fr = {
+    '01': 'Jan',
+    '02': 'Fév',
+    '03': 'Mar',
+    '04': 'Avr',
+    '05': 'Mai',
+    '06': 'Juin',
+    '07': 'Juil',
+    '08': 'Août',
+    '09': 'Sept',
+    '10': 'Oct',
+    '11': 'Nov',
+    '12': 'Déc'
+}
+
+def translate_month(month_num):
+    if month_num in month_names_fr:
+        return month_names_fr[month_num]
+    else:
+        return "Mois invalide"
+
+
+def cancel_record(driver):
+    text_to_click = "Annuler"
+    xpath = f"//span[text()='{text_to_click}']"
+    cancel = driver.find_element(By.XPATH, xpath)
+    cancel.click()
+    sleep(5)
+
+def find_element_with_retries(driver, by, value, retries=3, delay=1):
+    """Try to find an element with retries."""
+    for attempt in range(retries):
+        try:
+            return driver.find_element(by, value)
+        except NoSuchElementException:
+            logging.error(
+                f"Attempt {attempt + 1}/{retries}: Le bouton programmer un enregistrement n'a pas été trouvé."
+            )
+            sleep(delay)
+    logging.error(
+        "Impossible de trouver le bouton programmer un enregistrement après plusieurs tentatives."
+    )
+    driver.quit()
+    exit()
+
+if SENTRY_MONITORING_SDK:
+    sentry_sdk.init(
+        dsn="https://085eb844a807c3c997195cf7cd60a5a3@o4508778574381056.ingest.de.sentry.io/4508778965106768",
+        traces_sample_rate=0,
+    )
+    if sentry_sdk.Hub.current.client and sentry_sdk.Hub.current.client.options.get("traces_sample_rate", 0) > 0:
+        sentry_sdk.profiler.start_profiler()
+
+
+log_file = "/home/" + user + "/.local/share/select_freeboxos/logs/select_freeboxos.log"
+max_bytes = 10 * 1024 * 1024  # 10 MB
+backup_count = 5
+
+log_handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+log_format = '%(asctime)s %(levelname)s %(message)s'
+log_datefmt = '%d-%m-%Y %H:%M:%S'
+formatter = logging.Formatter(log_format, log_datefmt)
+
+log_handler.setFormatter(formatter)
+
+logger = logging.getLogger("module_freeboxos")
+logger.addHandler(log_handler)
+
+sentry_handler = logging.StreamHandler()
+sentry_handler.setLevel(logging.WARNING)
+
+logger.addHandler(sentry_handler)
+logger.setLevel(logging.INFO)
+
+logging.basicConfig(level=logging.INFO,
+                    format=log_format,
+                    datefmt=log_datefmt,
+                    handlers=[log_handler, sentry_handler])
+
+try:
+    with open(
+        "/home/" + user + "/.local/share/select_freeboxos/info_progs.json", "r"
+    ) as jsonfile:
+        data = json.load(jsonfile)
+except FileNotFoundError:
+    logging.error(
+        "No info_progs.json file. Need to check curl command or "
+        "internet connection. Exit programme."
+    )
+    exit()
+except json.JSONDecodeError:
+    logging.error(
+        "Invalid JSON data in info_progs.json file. The file may be empty or corrupted."
+    )
+    exit()
+
+if len(data) == 0:
+    src_file = os.path.join(os.path.expanduser("~"), ".local", "share", "select_freeboxos", "info_progs.json")
+    dst_file = os.path.join(os.path.expanduser("~"), ".local", "share", "select_freeboxos", "info_progs_last.json")
+    shutil.copy(src_file, dst_file)
+    logging.info("No data to record programmes. Exit programme.")
+    exit()
+
+if is_snap_installed() and is_firefox_snap():
+    service = Service(executable_path="/snap/bin/firefox.geckodriver")
+else:
+    service = Service(executable_path="/home/" + user + "/.local/share/select_freeboxos/geckodriver")
+options = webdriver.FirefoxOptions()
+options.add_argument("start-maximized")
+options.add_argument("--headless")
+
+if is_firefox_esr_installed():
+    options.binary_location = "/usr/bin/firefox-esr"
+
+if CRYPTED_CREDENTIALS:
+    try:
+        FREEBOX_SERVER_IP = keyring.get_password("freeboxos", "username")
+        ADMIN_PASSWORD = keyring.get_password("freeboxos", "password")
+        if FREEBOX_SERVER_IP is None:
+            logging.error("Failed to retrieve 'username' from keyring for 'freeboxos'.")
+            exit(1)
+        if ADMIN_PASSWORD is None:
+            logging.error("Failed to retrieve 'password' from keyring for 'freeboxos'.")
+            exit(1)
+    except Exception as e:
+        logging.exception("An error occurred while retrieving credentials from keyring.")
+        exit(1)
+
+try:
+    with webdriver.Firefox(service=service, options=options) as driver:
+        try:
+            driver.get("http://" + FREEBOX_SERVER_IP + "/login.php#Fbx.os.app.pvr.app")
+            sleep(8)
+        except WebDriverException as e:
+            if 'net::ERR_ADDRESS_UNREACHABLE' in e.msg:
+                logging.error(
+                    "The programme cannot reach the address " + FREEBOX_SERVER_IP + " . Exit programme."
+                )
+                driver.quit()
+                exit()
+            else:
+                logging.error("A WebDriverException occurred. Exiting the program.")
+                logging.error(f"Exception message: {e.msg}")
+                logging.error(f"Exception stack trace: {e.stacktrace}")
+                logging.error(f"Exception screenshot: {e.screen if hasattr(e, 'screen') else 'N/A'}")
+                logging.error(f"Exception log: {e.log if hasattr(e, 'log') else 'N/A'}")
+                driver.quit()
+                exit()
+
+        try:
+            login = driver.find_element("id", "fbx-password")
+        except Exception as e:
+            logging.error(
+                "Cannot connect to Freebox OS. Exit programme.", exc_info=True
+            )
+            driver.quit()
+            exit()
+        sleep(1)
+        login.click()
+        sleep(1)
+        login.send_keys(ADMIN_PASSWORD)
+        sleep(1)
+        login.send_keys(Keys.RETURN)
+        sleep(10)
+
+        try:
+            invalid_password = driver.find_element(
+                By.XPATH, "//div[contains(text(), 'Identifiants invalides')]"
+            )
+            logging.error(
+                "Le mot de passe administrateur de la Freebox est invalide. "
+                "La programmation des enregistrements n'a pas "
+                "pu être réalisée. Merci de vérifier le mot de passe."
+            )
+            driver.quit()
+            exit()
+        except NoSuchElementException:
+            pass
+
+
+        try:
+            with open(
+                "/home/" + user + "/.local/share/select_freeboxos/info_progs_last.json", "r"
+            ) as jsonfile:
+                data_last = json.load(jsonfile)
+        except FileNotFoundError:
+            data_last = []
+
+        starting = []
+
+        for video in data_last:
+            start = datetime.strptime(video["start"], "%Y%m%d%H%M").replace(
+                tzinfo=ZoneInfo("Europe/Paris")
+            )
+            end = start + timedelta(seconds=video["duration"])
+
+            starting.append((start, end))
+
+        n = 0
+        last_channel = "x/x"
+        start_last = None
+
+        for video in data:
+            n += 1
+
+            start = datetime.strptime(video["start"], "%Y%m%d%H%M").replace(
+                tzinfo=ZoneInfo("Europe/Paris")
+            )
+            if start_last is not None and start == start_last:
+                start += timedelta(minutes=1)
+
+            start_last = start
+            start_day = start.strftime("%d")
+            start_month = start.strftime("%m")
+            start_year = start.strftime("%y")
+            start_hour = start.strftime("%H")
+            start_minute = start.strftime("%M")
+
+            end = start + timedelta(seconds=video["duration"])
+            end_hour = end.strftime("%H")
+            end_minute = end.strftime("%M")
+
+            try:
+                channel_number = CHANNELS_FREE[video["channel"]]
+            except KeyError:
+                logging.error(
+                    "La chaine " + video["channel"] + " n'est pas "
+                    "présente dans le fichier channels_free.py"
+                )
+                continue
+
+            if len(starting) < MAX_SIM_RECORDINGS:
+                starting.append((start, end))
+                to_record = True
+            else:
+                if starting[-MAX_SIM_RECORDINGS][1] < start:
+                    starting.append((start, end))
+                    to_record = True
+                else:
+                    to_record = False
+
+            if to_record:
+                text_to_click = "Programmer un enregistrement"
+                xpath = f"//span[text()='{text_to_click}']"
+                programmer_enregistrements = find_element_with_retries(driver, By.XPATH, xpath)
+                sleep(1)
+                try:
+                    programmer_enregistrements.click()
+                except ElementClickInterceptedException as e:
+                    logging.error("A ElementClickInterceptedException occurred.")
+                    logging.error(f"Exception message: {e.msg}")
+                    logging.error(f"Exception stack trace: {e.stacktrace}")
+                    logging.error(
+                        "Impossible de programmer les enregistrements. "
+                        "Une fenêtre d'information empêche probablement "
+                        "de pouvoir clicker sur le bouton programmer un "
+                        "enregistrement."
+                    )
+                    driver.quit()
+                    exit()
+                sleep(3)
+                channel_uuid = driver.find_element("name", "channel_uuid")
+                sleep(1)
+                n = 0
+                follow_record = True
+                while channel_uuid.get_attribute("value").split("/")[0] != channel_number:
+                    channel_uuid.clear()
+                    sleep(1)
+                    if last_channel.split("/")[0] != channel_number:
+                        channel_uuid.send_keys(channel_number)
+                    else:
+                        channel_uuid.click()
+                        sleep(1)
+                        channel_uuid.clear()
+                        sleep(3)
+                        channel_uuid.send_keys(last_channel)
+                        sleep(1)
+                        channel_uuid.click()
+                    sleep(1)
+                    channel_uuid.send_keys(Keys.RETURN)
+                    sleep(1)
+                    last_channel = channel_uuid.get_attribute("value")
+                    n += 1
+                    if n > 10:
+                        logging.error(
+                            "Impossible de sélectionner la chaîne. Merci de "
+                            "vérifier si la chaine n°" + channel_number + " qui "
+                            "correspond à la chaine " + video["channel"] + " "
+                            "de MEDIA-select est bien présente dans la liste des "
+                            "chaines Freebox. "
+                        )
+                        follow_record = False
+                        break
+                if follow_record:
+                    date = driver.find_element("name", "date")
+                    date.click()
+                    sleep(1)
+                    text_to_click = start_day + " " + translate_month(start_month)
+                    xpath = f"//li[contains(text(), '{text_to_click}') and not(contains(text(), 'TV'))]"
+                    try:
+                        day_click = driver.find_element(By.XPATH, xpath)
+                    except NoSuchElementException as e:
+                        logging.error("A NoSuchElementException occurred.")
+                        logging.error(f"Exception message: {e.msg}")
+                        logging.error(f"Exception stack trace: {e.stacktrace}")
+                        logging.error(
+                            "Impossible de trouver la date pour le programme "
+                            + video["title"] + ". Le programme ne sera pas "
+                            "enregistré."
+                        )
+                        cancel_record(driver)
+                        continue
+                    day_click.click()
+                    sleep(1)
+                    to_cancel = False
+                    actual_start = "943463167"
+                    loop_counter = 0
+                    while True:
+                        start_time = driver.find_element("name", "start_time")
+                        start_time.clear()
+                        sleep(0.5)
+                        start_time.send_keys(start_hour + ":" + start_minute)
+                        try:
+                            WebDriverWait(driver, 10).until(
+                                lambda d: start_time.get_attribute("value") == start_hour + ":" + start_minute
+                            )
+                        except:
+                            logging.error("Timeout: The input field did not update to the correct time.")
+
+                        actual_start = start_time.get_attribute("value")
+
+                        if actual_start == start_hour + ":" + start_minute:
+                            break
+                        loop_counter += 1
+                        if loop_counter > 4:
+                            logging.error(
+                                f"Impossible de saisir l'heure de début pour le programme {video['title']}. Le programme ne sera pas enregistré."
+                            )
+                            to_cancel = True
+                            break
+                    sleep(1)
+                    start_time.send_keys(Keys.RETURN)
+                    sleep(1)
+                    actual_end = "943463167"
+                    loop_counter = 0
+                    while True:
+                        end_time = driver.find_element("name", "end_time")
+                        end_time.clear()
+                        sleep(0.5)
+                        end_time.send_keys(end_hour + ":" + end_minute)
+                        try:
+                            WebDriverWait(driver, 10).until(
+                                lambda d: end_time.get_attribute("value") == end_hour + ":" + end_minute
+                            )
+                        except:
+                            logging.error("Timeout: The input field did not update to the correct time.")
+
+                        actual_end = end_time.get_attribute("value")
+
+                        if actual_end == end_hour + ":" + end_minute:
+                            break
+                        loop_counter += 1
+                        if loop_counter > 4:
+                            logging.error(
+                                f"Impossible de saisir l'heure de fin pour le programme {video['title']}. Le programme ne sera pas enregistré."
+                            )
+                            to_cancel = True
+                            break
+                    if to_cancel:
+                        cancel_record(driver)
+                    else:
+                        sleep(1)
+                        end_time.send_keys(Keys.RETURN)
+                        sleep(1)
+                        if MEDIA_SELECT_TITLES:
+                            name_prog = driver.find_element("name", "name")
+                            try:
+                                name_prog.clear()
+                                sleep(1)
+                                name_prog.send_keys(video["title"])
+                                sleep(1)
+                            except ElementNotInteractableException:
+                                logging.error(
+                                    "Une ElementNotInteractableException est apparue. "
+                                    "Le titre de MEDIA select ne sera pas utilisé pour "
+                                    "nommer le vidéo."
+                                )
+                        text_to_click = "Sauvegarder"
+                        xpath = f"//span[text()='{text_to_click}']"
+                        sauvegarder = driver.find_element(By.XPATH, xpath)
+                        sauvegarder.click()
+                        sleep(5)
+                        try:
+                            internal_error = driver.find_element(
+                                By.XPATH, "//div[contains(text(), 'Erreur interne')]"
+                            )
+                            logging.error(
+                                "Une erreur interne de la Freebox est survenue. "
+                                "La programmation des enregistrements n'a pas "
+                                "pu être réalisée. Merci de vérifier si le disque "
+                                "dur n'est pas plein."
+                            )
+                            break
+                        except NoSuchElementException:
+                            pass
+                else:
+                    cancel_record(driver)
+
+        sleep(6)
+        driver.quit()
+
+        src_file = os.path.join(os.path.expanduser("~"), ".local", "share", "select_freeboxos", "info_progs.json")
+        dst_file = os.path.join(os.path.expanduser("~"), ".local", "share", "select_freeboxos", "info_progs_last.json")
+        shutil.copy(src_file, dst_file)
+except Exception as e:
+    logging.error("An unexpected error occurred:")
+    logging.error("Exception: %s", e)
+    logging.error("Stack trace:", exc_info=True)
---
+++ install.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+# Installation des librairies:
+
+echo -e "Installation des librairies nécessaires\n"
+
+if [ $(id -u) != 0 ] ; then
+  echo "Les droits Superuser (root) sont nécessaires pour installer select-freeboxos"
+  echo "Lancez 'sudo $0' pour obtenir les droits Superuser."
+  exit 1
+fi
+
+step_1_upgrade() {
+  echo "---------------------------------------------------------------------"
+  echo "Starting step 1 - install"
+  apt update
+  apt -y upgrade
+  echo "Step 1 - Install done"
+}
+
+step_2_mainpackage() {
+  echo "---------------------------------------------------------------------"
+  echo "Starting step 2 - packages"
+  apt -y install curl
+  apt -y install virtualenv
+  apt -y install unzip
+  apt -y install cron
+  apt -y install jq
+  hostname=$(uname -n)
+  codename=$(grep 'VERSION_CODENAME=' /etc/os-release | cut -d'=' -f2)
+  if [ $hostname = "nanopineo" ]
+  then
+    add-apt-repository -y ppa:mozillateam/ppa
+    apt update
+    apt -y install firefox-esr
+  # elif [ $hostname = "lepotato" -a $codename = "bookworm" ]
+  # then
+  #   echo "Purge firefox-esr already installed on Le potato cards with \
+  #   Armbian bookworm."
+  #   apt -y purge firefox-esr
+  #   echo "Install snapd and Firefox with Snap"
+  #   apt -y install snapd
+  #   systemctl start snapd
+  #   systemctl enable snapd
+  #   snap install firefox
+  elif [ $codename = "bookworm" ]
+  then
+    apt -y install firefox-esr
+  elif [ $codename = "noble" ]
+  then
+    # snap remove firefox
+    add-apt-repository -y ppa:mozillateam/ppa
+    apt update
+    apt -y install firefox-esr
+  elif [ $hostname = "raspbian-bullseye-aml-s905x-cc" -o $hostname = "NanoPi-NEO" ]
+  then
+    apt -y install software-properties-common
+    add-apt-repository -y ppa:mozillateam/ppa
+    apt update
+    apt -y install firefox-esr
+  else
+    apt -y install firefox
+  fi
+  echo "step 2 - packages done"
+}
+
+step_3_freeboxos_download() {
+  echo "---------------------------------------------------------------------"
+  echo "Starting step 3 - freeboxos download"
+  HOME_DIR=$(eval echo ~$SUDO_USER)
+  cd "$HOME_DIR" && curl https://github.com/mediaselect/select-freeboxos-sbc-vm/archive/refs/tags/v1.0.0.zip -L -o select_freebox.zip
+  selectos=$(ls "$HOME_DIR" | grep select-freeboxos)
+  pretty=$(grep 'PRETTY_NAME=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
+  if [ -n "$selectos" ]
+  then
+    rm -rf "$HOME_DIR"/select-freeboxos
+  fi
+  unzip select_freebox.zip && mv select-freeboxos-sbc-vm-1.0.0 select-freeboxos && rm select_freebox.zip
+  chown -R "$SUDO_USER:$SUDO_USER" "$HOME_DIR/select-freeboxos"
+  echo "Step 3 - freeboxos download done"
+}
+
+arch32=("AArch32" "arm" "ARMv1" "ARMv2" "ARMv3" "ARMv4" "ARMv5" "ARMv6" "ARMv7")
+
+arch64=("AArch64" "arm64" "ARMv8" "ARMv9")
+
+info_not_arm=false
+
+step_4_create_select_freeboxos_directories() {
+  echo "---------------------------------------------------------------------"
+  echo "Starting step 4 - Creating .local/share/select_freeboxos"
+  user=$(who am i | awk '{print $1}')
+  if [ ! -d /home/$user/.local ]; then
+    sudo -u $user mkdir /home/$user/.local
+    sudo -u $user chmod 700 /home/$user/.local
+  fi
+  if [ ! -d /home/$user/.local/share ]; then
+    sudo -u $user mkdir /home/$user/.local/share
+    sudo -u $user chmod 700 /home/$user/.local/share
+  fi
+  if [ ! -d /home/$user/.config ]; then
+    sudo -u $user mkdir /home/$user/.config
+    sudo -u $user chmod 700 /home/$user/.config
+  fi
+  sudo -u $user mkdir -p /home/$user/.local/share/select_freeboxos/logs
+  sudo -u $user chmod -R 740 /home/$user/.local/share/select_freeboxos
+  sudo -u $user mkdir -p /home/$user/.config/select_freeboxos
+  sudo -u $user chmod -R 740 /home/$user/.config/select_freeboxos
+  echo "Step 4 - select_freeboxos directories created"
+}
+
+step_5_geckodriver_download() {
+  echo "---------------------------------------------------------------------"
+  echo "Starting step 5 - geckodriver download"
+  cd /home/$user/.local/share/select_freeboxos
+  cpu=$(lscpu | grep Architecture | awk {'print $2'})
+  cpu_lower=$(echo "$cpu" | tr '[:upper:]' '[:lower:]')
+  cpu_five_chars="${cpu_lower:0:5}"
+
+  if echo "${arch64[@],,}" | grep -q "$cpu_five_chars"
+  then
+    wget https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-linux-aarch64.tar.gz
+    sudo -u $user bash -c "tar xzvf geckodriver-v0.34.0-linux-aarch64.tar.gz"
+  elif echo "${arch32[@],,}" | grep -q "$cpu_five_chars"
+  then
+    wget https://github.com/jamesmortensen/geckodriver-arm-binaries/releases/download/v0.34.0/geckodriver-v0.34.0-linux-armv7l.tar.gz
+    sudo -u $user bash -c "tar xzvf geckodriver-v0.34.0-linux-armv7l.tar.gz"
+  else
+    info_not_arm=true
+  echo "Step 5 - geckodriver download done"
+  fi
+}
+
+
+step_6_virtual_environment() {
+  echo "---------------------------------------------------------------------"
+  echo "Starting step 6 - Virtual env + requirements install"
+  sudo -u $user bash -c "virtualenv -p python3 .venv"
+  sudo -u $user bash -c "source .venv/bin/activate && pip install -r /home/$user/select-freeboxos/requirements.txt"
+  echo "Step 6 - Virtual env created and requirements installed"
+}
+
+
+STEP=0
+
+case ${STEP} in
+  0)
+  echo "Starting installation ..."
+  step_1_upgrade
+  step_2_mainpackage
+  step_3_freeboxos_download
+  step_4_create_select_freeboxos_directories
+  step_5_geckodriver_download
+  step_6_virtual_environment
+  ;;
+esac
+
+if $info_not_arm
+then
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "Geckodriver n'a pas pu être téléchargé car votre architecture \
+CPU est différente de ARM. Le programme ne peut pas \
+fonctionner sans geckodriver. Contactez TV-select pour obtenir le \
+geckodriver qui correspond à votre architecture CPU."
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+fi
