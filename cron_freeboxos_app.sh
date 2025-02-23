12921bbdf30983715ae342418ae15be8d851655d
tvselect <tv.select.fr@gmail.com>
2025-02-23 07:12:06 +0100

    Install branch initial commit

--- cron_freeboxos_app.sh
+++
@@ -1,6 +0,0 @@
-echo --- crontab start: $(date) >> /home/$USER/.local/share/select_freeboxos/logs/cron_freeboxos.log
-cd /home/$USER/select-freeboxos
-. /home/$USER/.local/share/select_freeboxos/.venv/bin/activate
-export DISPLAY=:0 && python3 freeboxos.py >> /home/$USER/.local/share/select_freeboxos/logs/cron_freeboxos.log 2>&1
-deactivate
-echo --- crontab end: $(date) >> /home/$USER/.local/share/select_freeboxos/logs/cron_freeboxos.log
