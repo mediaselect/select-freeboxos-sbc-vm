ef83b946e45c312e3123185964cd5c4199fd2a16
tvselect <tv.select.fr@gmail.com>
2026-09-05 21:53:45 +0200

    installer: switch default download from v3.0.0 to v3.0.1

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ step_2_mainpackage() {
 step_3_freeboxos_download() {
   echo "---------------------------------------------------------------------"
   echo "Starting step 3 - freeboxos download"
-  cd "$HOME_DIR" && curl https://github.com/mediaselect/select-freeboxos-sbc-vm/archive/refs/tags/v3.0.0.zip -L -o select_freebox.zip
+  cd "$HOME_DIR" && curl https://github.com/mediaselect/select-freeboxos-sbc-vm/archive/refs/tags/v3.0.1.zip -L -o select_freebox.zip
   SELECT_FREEBOXOS_SHA256="34faa340b58259645c8c1eb222502e1beff6f9272050a8eaa4464a00c0adcc20"
   if ! echo "$SELECT_FREEBOXOS_SHA256  select_freebox.zip" | sha256sum -c -; then
       echo "ERROR: Checksum verification failed for select_freebox.zip!"
@@ -86,7 +86,7 @@ step_3_freeboxos_download() {
   if [ -d "$HOME_DIR/select-freeboxos" ]; then
       rm -rf "$HOME_DIR/select-freeboxos"
   fi
-  unzip select_freebox.zip && mv select-freeboxos-sbc-vm-3.0.0 select-freeboxos && rm select_freebox.zip
+  unzip select_freebox.zip && mv select-freeboxos-sbc-vm-3.0.1 select-freeboxos && rm select_freebox.zip
   chown -R "$SUDO_USER:$SUDO_USER" "$HOME_DIR/select-freeboxos"
   echo "Step 3 - freeboxos download done"
 }
